4c7e39a356bc31fdd56545171e2b9acd3c069531
Peter Wang <novalazy@gmail.com>
2011-08-21 16:34:00 +1000

    Add popen() interface.

---
+++ popen.m
@@ -0,0 +1,86 @@
+%-----------------------------------------------------------------------------%
+
+:- module popen.
+:- interface.
+
+:- import_module io.
+
+:- pred popen(string::in, io.res(string)::out, io::di, io::uo) is det.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module list.
+:- import_module string.
+
+:- pragma foreign_decl("C", local, "
+    #include <stdio.h>
+").
+
+%-----------------------------------------------------------------------------%
+
+popen(Command, Res, !IO) :-
+    popen_2(Command, Errno, RevList, !IO),
+    ( Errno = 0 ->
+        ( RevList = [String0] ->
+            String = String0
+        ;
+            list.reverse(RevList, List),
+            string.append_list(List, String)
+        ),
+        Res = ok(String)
+    ;
+        Res = error(io.make_io_error(strerror(Errno)))
+    ).
+
+:- pred popen_2(string::in, int::out, list(string)::uo, io::di, io::uo) is det.
+
+:- pragma foreign_proc("C",
+    popen_2(Command::in, Errno::out, RevList::uo, IO0::di, IO::uo),
+    [will_not_call_mercury, promise_pure, thread_safe, tabled_for_io,
+        may_not_duplicate],
+"
+    static char buf[16384];
+    FILE *fp;
+    int n;
+    MR_String s;
+
+    RevList = MR_list_empty();
+
+    fp = popen(Command, ""r"");
+
+    if (fp) {
+        for (;;) {
+            n = fread(buf, 1, sizeof(buf) - 1, fp);
+            if (n < 0) {
+                Errno = errno;
+                break;
+            } else if (n == 0) {
+                Errno = 0;
+                break;
+            } else {
+                buf[n] = '\\0';
+                MR_make_aligned_string_copy(s, buf);
+                RevList = MR_list_cons(s, RevList); 
+            }
+        }
+        pclose(fp);
+    } else {
+        Errno = errno;
+    }
+    IO = IO0;
+").
+
+:- func strerror(int::in) = (string::uo) is det.
+
+:- pragma foreign_proc("C",
+    strerror(Errno::in) = (String::uo),
+    [will_not_call_mercury, promise_pure, not_thread_safe, may_not_duplicate],
+"
+    MR_make_aligned_string_copy(String, strerror(Errno));
+").
+
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sts=4 sw=4 et
