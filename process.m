b3abbf3490ba725aaf12446e5ca2a42fcd90f6da
Peter Wang <novalazy@gmail.com>
2012-05-07 22:37:54 +1000

    process: Add option to capture stdout from child process.

--- process.m
+++ process.m
@@ -10,8 +10,13 @@
 :- type pid
     --->    pid(int).
 
+:- type pipe_read.
+
 :- pred posix_spawn(string::in, list(string)::in, io.res(pid)::out,
     io::di, io::uo) is det.
+
+:- pred posix_spawn_capture_stdout(string::in, list(string)::in,
+    io.res({pid, pipe_read})::out, io::di, io::uo) is det.
 
 :- type wait_pid_blocking
     --->    blocking
@@ -27,6 +32,9 @@
 :- pred wait_pid(pid::in, wait_pid_blocking::in, wait_pid_result::out,
     io::di, io::uo) is det.
 
+:- pred drain_and_close_pipe(pipe_read::in, io.res(string)::out,
+    io::di, io::uo) is det.
+
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
 
@@ -34,31 +42,47 @@
 
 :- import_module bool.
 :- import_module int.
+:- import_module string.
+
+:- type pipe_read
+    --->    pipe_read(
+                fd  :: int
+            ).
 
 %-----------------------------------------------------------------------------%
 
 posix_spawn(Prog, Args, Res, !IO) :-
     list.length(Args, NumArgs),
-    posix_spawn_2(Prog, Args, NumArgs, Pid, !IO),
+    CaptureStdout = no,
+    posix_spawn_2(Prog, Args, NumArgs, CaptureStdout, Pid, _Fd, !IO),
     ( Pid >= 0 ->
         Res = ok(pid(Pid))
     ;
         Res = error(io.make_io_error("posix_spawn failed"))
     ).
 
-:- pred posix_spawn_2(string::in, list(string)::in, int::in, int::out,
-    io::di, io::uo) is det.
+posix_spawn_capture_stdout(Prog, Args, Res, !IO) :-
+    list.length(Args, NumArgs),
+    CaptureStdout = yes,
+    posix_spawn_2(Prog, Args, NumArgs, CaptureStdout, Pid, Fd, !IO),
+    ( Pid >= 0 ->
+        Res = ok({pid(Pid), pipe_read(Fd)})
+    ;
+        Res = error(io.make_io_error("posix_spawn failed"))
+    ).
+
+:- pred posix_spawn_2(string::in, list(string)::in, int::in, bool::in,
+    int::out, int::out, io::di, io::uo) is det.
 
 :- pragma foreign_proc("C",
-    posix_spawn_2(Prog::in, Args::in, NumArgs::in, Pid::out,
-        _IO0::di, _IO::uo),
+    posix_spawn_2(Prog::in, Args::in, NumArgs::in, CaptureStdout::in,
+        Pid::out, PipeRead::out, _IO0::di, _IO::uo),
     [will_not_call_mercury, promise_pure, thread_safe, tabled_for_io,
         may_not_duplicate],
 "
     pid_t pid;
-    posix_spawn_file_actions_t file_actions;
-    posix_spawnattr_t attr;
     char *argv[NumArgs + 1];    /* C99 stack allocation */
+    int pipefd[2] = {-1, -1};
     int i;
     int rc;
 
@@ -70,24 +94,69 @@ posix_spawn(Prog, Args, Res, !IO) :-
     }
     argv[i] = NULL;
 
+    rc = 0;
+
+    if (CaptureStdout) {
+        rc = pipe(pipefd);
+    }
+
+    if (rc == 0) {
+        rc = do_posix_spawn(&pid, Prog, argv, pipefd);
+    }
+
+    if (rc == 0) {
+        Pid = pid;
+        PipeRead = pipefd[0];
+        /* Close the write end of the pipe in the parent. */
+        if (pipefd[1] != -1) {
+            close(pipefd[1]);
+        }
+    } else {
+        Pid = -1;
+        if (pipefd[0] != -1) {
+            close(pipefd[0]);
+        }
+        if (pipefd[1] != -1) {
+            close(pipefd[1]);
+        }
+    }
+").
+
+:- pragma foreign_decl("C", local, "
+static int
+do_posix_spawn(pid_t *pid_ptr, const char *Prog, char *argv[], int pipefd[2])
+{
+    posix_spawn_file_actions_t file_actions;
+    posix_spawnattr_t attr;
+    int rc;
+
+    posix_spawn_file_actions_init(&file_actions);
+
     /*
     ** Close stdin in the child.  This prevents interference with reading
     ** input in the parent process.
     */
-    posix_spawn_file_actions_init(&file_actions);
     posix_spawn_file_actions_addclose(&file_actions, STDIN_FILENO);
+
+    /*
+    ** Close the read end of the pipe in the child, then redirect
+    ** stdout to the write end of the pipe.
+    */
+    if (pipefd[0] != -1) {
+        posix_spawn_file_actions_addclose(&file_actions, pipefd[0]);
+        posix_spawn_file_actions_adddup2(&file_actions, pipefd[1],
+            STDOUT_FILENO);
+    }
+
     posix_spawnattr_init(&attr);
 
-    rc = posix_spawnp(&pid, Prog, &file_actions, &attr, argv, environ);
+    rc = posix_spawnp(pid_ptr, Prog, &file_actions, &attr, argv, environ);
 
     posix_spawnattr_destroy(&attr);
     posix_spawn_file_actions_destroy(&file_actions);
 
-    if (rc == 0) {
-        Pid = pid;
-    } else {
-        Pid = -1;
-    }
+    return rc;
+}
 ").
 
 %-----------------------------------------------------------------------------%
@@ -150,6 +219,51 @@ wait_pid(pid(Pid), Blocking, Res, !IO) :-
         ExitStatus = -1;
         Signal = -1;
     }
+").
+
+%-----------------------------------------------------------------------------%
+
+drain_and_close_pipe(pipe_read(Fd), Res, !IO) :-
+    drain_and_close_pipe_2(Fd, Error, RevList, !IO),
+    ( Error \= "" ->
+        Res = error(io.make_io_error(Error))
+    ; RevList = [String] ->
+        Res = ok(String)
+    ;
+        list.reverse(RevList, List),
+        string.append_list(List, String),
+        Res = ok(String)
+    ).
+
+:- pred drain_and_close_pipe_2(int::in, string::out, list(string)::uo,
+    io::di, io::uo) is det.
+
+:- pragma foreign_proc("C",
+    drain_and_close_pipe_2(Fd::in, Error::out, RevList::uo, _IO0::di, _IO::uo),
+    [will_not_call_mercury, promise_pure, tabled_for_io, may_not_duplicate],
+"
+    static char buf[16384];
+    MR_String s;
+    int n;
+
+    RevList = MR_list_empty();
+
+    for (;;) {
+        n = read(Fd, buf, sizeof(buf));
+        if (n < 0) {
+            Error = MR_make_string(MR_ALLOC_ID, ""%s"", strerror(errno));
+            break;
+        } else if (n == 0) {
+            Error = MR_make_string_const("""");
+            break;
+        } else {
+            buf[n] = '\\0';
+            MR_make_aligned_string_copy_msg(s, buf, MR_ALLOC_ID);
+            RevList = MR_list_cons((MR_Word)s, RevList);
+        }
+    }
+
+    close(Fd);
 ").
 
 %-----------------------------------------------------------------------------%
