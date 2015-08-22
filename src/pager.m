81e0fa535e67cf13e7942f150f5aaeddb880e6cb
Peter Wang <novalazy@gmail.com>
2015-08-22 12:07:43 +1000

    Move some code into new module pager_text.m

--- src/pager.m
+++ src/pager.m
@@ -118,9 +118,9 @@
 :- import_module version_array.
 
 :- import_module copious_output.
+:- import_module pager_text.
 :- import_module quote_arg.
 :- import_module string_util.
-:- import_module uri.
 
 %-----------------------------------------------------------------------------%
 
@@ -149,7 +149,7 @@
 :- type pager_line
     --->    start_message_header(message, string, header_value)
     ;       header(string, header_value)
-    ;       text(quote_level, string, quote_marker_end, text_type)
+    ;       text(pager_text)
     ;       part_head(
                 part            :: part,
                 alternatives    :: list(part),
@@ -161,22 +161,6 @@
                 expanded        :: bool
             )
     ;       message_separator.
-
-:- type quote_level == int.
-
-:- type quote_marker_end == int.
-
-:- type text_type
-    --->    plain
-    ;       diff(diff_line)
-    ;       url(int, int).  % (start, end] columns
-
-:- type diff_line
-    --->    diff_common
-    ;       diff_add
-    ;       diff_rem
-    ;       diff_hunk
-    ;       diff_index.
 
 :- type part_expanded
     --->    part_expanded
@@ -249,7 +233,7 @@ make_id_pager_line(NodeId, Line) = NodeId - Line.
 
 :- pred is_blank_line(pager_line::in) is semidet.
 
-is_blank_line(text(_, "", _, _)).
+is_blank_line(text(pager_text(_, "", _, _))).
 is_blank_line(message_separator).
 
 :- pred replace_node(node_id::in, tree::in, tree::in, tree::out) is det.
@@ -372,7 +356,8 @@ make_part_tree_with_alts(Config, Cols, AltParts, Part, ExpandUnsupported, Tree,
     allocate_node_id(PartNodeId, !Counter),
     (
         Content = text(Text),
-        make_text_lines(Cols, Text, Lines),
+        make_text_lines(Cols, Text, Lines0),
+        list.map(wrap_text, Lines0) = Lines,
         fold_quote_blocks(Lines, TextTrees, !Counter),
         (
             !.ElideInitialHeadLine = yes,
@@ -450,285 +435,6 @@ hide_multipart_head_line(PartType) :-
 
 %-----------------------------------------------------------------------------%
 
-:- type append_text_context
-    --->    append_text_context(
-                % Current quote level (for wrapped lines).
-                atc_cur_quote_level     :: maybe(quote_level),
-                % Current line type (for wrapped lines).
-                atc_cur_line_type       :: maybe(text_type),
-                % Known diffs at these quote levels.
-                atc_diff_quote_levels   :: set(quote_level)
-            ).
-
-:- pred make_text_lines(int::in, string::in, list(pager_line)::out) is det.
-
-make_text_lines(Max, String, Lines) :-
-    Start = 0,
-    LastBreak = 0,
-    Cur = 0,
-    QuoteLevel = no,
-    DiffLine = no,
-    DiffQuoteLevels = set.init,
-    Context0 = append_text_context(QuoteLevel, DiffLine, DiffQuoteLevels),
-    append_text(Max, String, Start, LastBreak, Cur, Context0, _Context,
-        cord.init, LinesCord),
-    Lines = cord.list(LinesCord).
-
-:- pred append_text(int::in, string::in, int::in, int::in, int::in,
-    append_text_context::in, append_text_context::out,
-    cord(pager_line)::in, cord(pager_line)::out) is det.
-
-append_text(Max, String, Start, LastBreak, Cur, !Context, !Lines) :-
-    ( string.unsafe_index_next(String, Cur, Next, Char) ->
-        (
-            Char = '\n'
-        ->
-            append_substring(String, Start, Cur, !Context, !Lines),
-            reset_context(!Context),
-            append_text(Max, String, Next, Next, Next, !Context, !Lines)
-        ;
-            char.is_whitespace(Char)
-        ->
-            append_text(Max, String, Start, Cur, Next, !Context, !Lines)
-        ;
-            % Wrap long lines.
-            % XXX this should actually count with wcwidth
-            Next - Start > Max
-        ->
-            maybe_append_substring(String, Start, LastBreak, !Context, !Lines),
-            skip_whitespace(String, LastBreak, NextStart),
-            append_text(Max, String, NextStart, NextStart, Next, !Context,
-                !Lines)
-        ;
-            append_text(Max, String, Start, LastBreak, Next, !Context, !Lines)
-        )
-    ;
-        % End of string.
-        maybe_append_substring(String, Start, Cur, !Context, !Lines)
-    ).
-
-:- pred reset_context(append_text_context::in, append_text_context::out)
-    is det.
-
-reset_context(!Context) :-
-    !Context ^ atc_cur_quote_level := no,
-    !Context ^ atc_cur_line_type := no.
-
-:- pred maybe_append_substring(string::in, int::in, int::in,
-    append_text_context::in, append_text_context::out,
-    cord(pager_line)::in, cord(pager_line)::out) is det.
-
-maybe_append_substring(String, Start, End, !Context, !Lines) :-
-    ( End > Start ->
-        append_substring(String, Start, End, !Context, !Lines)
-    ;
-        true
-    ).
-
-:- pred append_substring(string::in, int::in, int::in,
-    append_text_context::in, append_text_context::out,
-    cord(pager_line)::in, cord(pager_line)::out) is det.
-
-append_substring(String, Start, End, Context0, Context, !Lines) :-
-    Context0 = append_text_context(MaybeQuoteLevel0, MaybeLineType0,
-        DiffQuoteLevels0),
-    string.unsafe_between(String, Start, End, SubString),
-    (
-        MaybeQuoteLevel0 = yes(QuoteLevel),
-        QuoteMarkerEnd = 0
-    ;
-        MaybeQuoteLevel0 = no,
-        detect_quote_level(SubString, 0, QuoteLevel, QuoteMarkerEnd)
-    ),
-    (
-        MaybeLineType0 = yes(diff(DiffLine))
-    ->
-        % Maintain diff line type across wrapped diff lines.
-        LineType = diff(DiffLine),
-        DiffQuoteLevels = DiffQuoteLevels0
-    ;
-        ( contains(DiffQuoteLevels0, QuoteLevel) ->
-            (
-                QuoteLevel = 0,
-                MaybeLineType0 = no,
-                detect_diff_end(String, Start)
-            ->
-                PrevDiff = no,
-                delete(QuoteLevel, DiffQuoteLevels0, DiffQuoteLevels1)
-            ;
-                PrevDiff = yes,
-                DiffQuoteLevels1 = DiffQuoteLevels0
-            )
-        ;
-            PrevDiff = no,
-            DiffQuoteLevels1 = DiffQuoteLevels0
-        ),
-        (
-            % Only detect diffs at start of line.
-            MaybeLineType0 = no,
-            detect_diff(String, Start + QuoteMarkerEnd, End, PrevDiff,
-                QuoteLevel, DiffLine)
-        ->
-            LineType = diff(DiffLine),
-            insert(QuoteLevel, DiffQuoteLevels1, DiffQuoteLevels)
-        ;
-            PrevDiff = yes
-        ->
-            LineType = diff(diff_common),
-            DiffQuoteLevels = DiffQuoteLevels0
-        ;
-            detect_url(SubString, UrlStart, UrlEnd)
-        ->
-            LineType = url(UrlStart, UrlEnd),
-            DiffQuoteLevels = DiffQuoteLevels0
-        ;
-            LineType = plain,
-            DiffQuoteLevels = DiffQuoteLevels0
-        )
-    ),
-    Text = text(QuoteLevel, SubString, QuoteMarkerEnd, LineType),
-    snoc(Text, !Lines),
-    Context = append_text_context(yes(QuoteLevel), yes(LineType),
-        DiffQuoteLevels).
-
-%-----------------------------------------------------------------------------%
-
-:- pred detect_quote_level(string::in, int::in, int::out, int::out) is det.
-
-detect_quote_level(String, Pos0, QuoteLevel, QuoteMarkerEnd) :-
-    ( quote_marker(String, Pos0, Pos1) ->
-        detect_quote_level(String, Pos1, QuoteLevel0, QuoteMarkerEnd),
-        QuoteLevel = 1 + QuoteLevel0
-    ;
-        QuoteLevel = 0,
-        QuoteMarkerEnd = Pos0
-    ).
-
-:- pred quote_marker(string::in, int::in, int::out) is semidet.
-
-quote_marker(String, !Pos) :-
-    string.unsafe_index_next(String, !Pos, Char),
-    (
-        Char = ('>')
-    ;
-        % Accept a single optional space before the quote character.
-        Char = (' '),
-        string.unsafe_index_next(String, !Pos, '>')
-    ).
-
-:- pred detect_diff(string::in, int::in, int::in, bool::in, quote_level::in,
-    diff_line::out) is semidet.
-
-detect_diff(String, I, CurLineEnd, PrevDiff, QuoteLevel, Diff) :-
-    string.unsafe_index_next(String, I, J, Char),
-    (
-        Char = ('+'),
-        (
-            PrevDiff = yes
-        ;
-            QuoteLevel > 0,
-            skip_nls(String, CurLineEnd, NextLineOffset),
-            lookahead_likely_diff(String, NextLineOffset, QuoteLevel)
-        ),
-        Diff = diff_add
-    ;
-        Char = ('-'),
-        ( J = CurLineEnd ->
-            true
-        ;
-            not dashes_or_sig_separator(String, J, CurLineEnd)
-        ),
-        (
-            PrevDiff = yes
-        ;
-            QuoteLevel > 0,
-            skip_nls(String, CurLineEnd, NextLineOffset),
-            lookahead_likely_diff(String, NextLineOffset, QuoteLevel)
-        ),
-        Diff = diff_rem
-    ;
-        Char = ('@'),
-        unsafe_substring_prefix(String, I, "@@ "),
-        Diff = diff_hunk
-    ;
-        Char = 'd',
-        unsafe_substring_prefix(String, I, "diff -"),
-        Diff = diff_index
-    ;
-        Char = 'I',
-        unsafe_substring_prefix(String, I, "Index: "),
-        Diff = diff_index
-    ).
-
-    % Quoted diffs are often missing the standard diff headers.  To help
-    % detect diff lines without diff headers, we look ahead one line (only).
-    %
-:- pred lookahead_likely_diff(string::in, int::in, quote_level::in) is semidet.
-
-lookahead_likely_diff(String, Start, ExpectedQuoteLevel) :-
-    detect_quote_level(String, Start, ExpectedQuoteLevel, QuoteMarkerEnd),
-    I = QuoteMarkerEnd,
-    string.unsafe_index_next(String, I, _, Char),
-    (
-        Char = ('+')
-    ;
-        Char = ('-')
-    ;
-        Char = ('@'),
-        unsafe_substring_prefix(String, I, "@@ ")
-    ;
-        Char = 'd',
-        unsafe_substring_prefix(String, I, "diff -")
-    ;
-        Char = 'I',
-        unsafe_substring_prefix(String, I, "Index: ")
-    ).
-
-:- pred dashes_or_sig_separator(string::in, int::in, int::in) is semidet.
-
-dashes_or_sig_separator(String, I0, End) :-
-    ( I0 < End ->
-        string.unsafe_index_next(String, I0, I1, Char),
-        (
-            Char = ('-'),
-            dashes_or_sig_separator(String, I1, End)
-        ;
-            Char = (' '),
-            I1 = End
-        )
-    ;
-        true
-    ).
-
-:- pred skip_nls(string::in, int::in, int::out) is det.
-
-skip_nls(String, I0, I) :-
-    ( string.unsafe_index_next(String, I0, I1, Char) ->
-        ( Char = ('\n') ->
-            skip_nls(String, I1, I)
-        ;
-            I = I0
-        )
-    ;
-        I = I0
-    ).
-
-:- pred detect_diff_end(string::in, int::in) is semidet.
-
-detect_diff_end(String, Start) :-
-    % At quote level zero diffs are likely posted in full and have a limited
-    % set of initial characters.
-    string.unsafe_index_next(String, Start, _, Char),
-    Char \= ('+'),
-    Char \= ('-'),
-    Char \= (' '),
-    Char \= ('@'),
-    Char \= ('d'),
-    Char \= ('I'),
-    Char \= ('i').
-
-%-----------------------------------------------------------------------------%
-
 :- pred fold_quote_blocks(list(pager_line)::in, list(tree)::out,
     counter::in, counter::out) is det.
 
@@ -775,7 +481,7 @@ is_not_quoted_text(Line) :-
 is_quoted_text(Line) :-
     require_complete_switch [Line]
     (
-        Line = text(Level, _, _, _),
+        Line = text(pager_text(Level, _, _, _)),
         Level > 0
     ;
         ( Line = start_message_header(_, _, _)
@@ -830,7 +536,7 @@ add_encapsulated_header(Header, Value, RevLines0, RevLines) :-
     ( ValueString = "" ->
         RevLines = RevLines0
     ;
-        Line = text(0, Header ++ ": " ++ ValueString, 0, plain),
+        Line = text(pager_text(0, Header ++ ": " ++ ValueString, 0, plain)),
         RevLines = [Line | RevLines0]
     ).
 
@@ -873,7 +579,7 @@ make_unsupported_part_tree(Config, Cols, PartNodeId, Part, ExpandUnsupported,
         TextLines = []
     ),
     HeadLine = part_head(Part, AltParts, Expanded),
-    Lines = [HeadLine | TextLines],
+    Lines = [HeadLine | wrap_texts(TextLines)],
     Tree = node(PartNodeId, [leaf(Lines)], yes).
 
 :- func expand_unsupported_default(bool) = part_expanded.
@@ -902,13 +608,21 @@ maybe_filter_command(Config, IsHtml) = MaybeFilterCommand :-
 
 :- func blank_line = pager_line.
 
-blank_line = text(0, "", 0, plain).
+blank_line = text(pager_text(0, "", 0, plain)).
+
+:- func wrap_texts(list(pager_text)) = list(pager_line).
+
+wrap_texts(Texts) = map(wrap_text, Texts).
+
+:- func wrap_text(pager_text) = pager_line.
+
+wrap_text(Text) = text(Text).
 
 %-----------------------------------------------------------------------------%
 
 setup_pager_for_staging(Config, Cols, Text, RetainPagerPos, Info) :-
     make_text_lines(Cols, Text, Lines0),
-    Lines = Lines0 ++ [
+    Lines = wrap_texts(Lines0) ++ [
         message_separator,
         message_separator,
         message_separator
@@ -1181,7 +895,7 @@ is_quoted_text_or_message_start_2(Line) :-
     (
         Line = start_message_header(_, _, _)
     ;
-        Line = text(Level, _, _, _),
+        Line = text(pager_text(Level, _, _, _)),
         Level > 0
     ;
         ( Line = header(_, _)
@@ -1342,7 +1056,7 @@ line_matches_search(Search, Line) :-
         String = header_value_string(Value),
         strcase_str(String, Search)
     ;
-        Line = text(_, String, _, _),
+        Line = text(pager_text(_, String, _, _)),
         strcase_str(String, Search)
     ;
         Line = part_head(Part, _, _),
@@ -1406,7 +1120,7 @@ do_highlight(Highlightable, NumRows, !Info) :-
 
 is_highlightable_minor(_Id - Line) :-
     ( Line = part_head(_, _, _)
-    ; Line = text(_, _, _, url(_, _))
+    ; Line = text(pager_text(_, _, _, url(_, _)))
     ; Line = fold_marker(_, _)
     ).
 
@@ -1433,7 +1147,7 @@ get_highlighted_thing(Info, Thing) :-
         MaybeSubject = no,
         Thing = highlighted_part(Part, MaybeSubject)
     ;
-        Line = text(_, String, _, url(Start, End)),
+        Line = text(pager_text(_, String, _, url(Start, End))),
         string.between(String, Start, End, Url),
         Thing = highlighted_url(Url)
     ;
@@ -1473,7 +1187,7 @@ toggle_line(ToggleType, Cols, NodeId - Line, MessageUpdate, !Info, !IO) :-
     ;
         ( Line = start_message_header(_, _, _)
         ; Line = header(_, _)
-        ; Line = text(_, _, _, _)
+        ; Line = text(pager_text(_, _, _, _))
         ; Line = message_separator
         ),
         MessageUpdate = clear_message
@@ -1609,7 +1323,7 @@ draw_pager_line(Attrs, Panel, Line, IsCursor, !IO) :-
         ),
         draw(Panel, BodyAttr + Highlight, header_value_string(Value), !IO)
     ;
-        Line = text(QuoteLevel, Text, QuoteMarkerEnd, TextType),
+        Line = text(pager_text(QuoteLevel, Text, QuoteMarkerEnd, TextType)),
         Attr0 = quote_level_to_attr(Attrs, QuoteLevel),
         (
             IsCursor = yes,
---
+++ src/pager_text.m
@@ -0,0 +1,327 @@
+% Bower - a frontend for the Notmuch email system
+% Copyright (C) 2015 Peter Wang
+
+:- module pager_text.
+:- interface.
+
+:- import_module list.
+
+%---------------------------------------------------------------------------%
+
+:- type pager_text
+    --->    pager_text(quote_level, string, quote_marker_end, text_type).
+
+:- type quote_level == int.
+
+:- type quote_marker_end == int.
+
+:- type text_type
+    --->    plain
+    ;       diff(diff_line)
+    ;       url(int, int).  % (start, end] columns
+
+:- type diff_line
+    --->    diff_common
+    ;       diff_add
+    ;       diff_rem
+    ;       diff_hunk
+    ;       diff_index.
+
+:- pred make_text_lines(int::in, string::in, list(pager_text)::out) is det.
+
+%-----------------------------------------------------------------------------%
+%-----------------------------------------------------------------------------%
+
+:- implementation.
+
+:- import_module bool.
+:- import_module char.
+:- import_module cord.
+:- import_module int.
+:- import_module maybe.
+:- import_module set.
+:- import_module string.
+
+:- import_module string_util.
+:- import_module uri.
+
+:- type append_text_context
+    --->    append_text_context(
+                % Current quote level (for wrapped lines).
+                atc_cur_quote_level     :: maybe(quote_level),
+                % Current line type (for wrapped lines).
+                atc_cur_line_type       :: maybe(text_type),
+                % Known diffs at these quote levels.
+                atc_diff_quote_levels   :: set(quote_level)
+            ).
+
+%---------------------------------------------------------------------------%
+
+make_text_lines(Max, String, Lines) :-
+    Start = 0,
+    LastBreak = 0,
+    Cur = 0,
+    QuoteLevel = no,
+    DiffLine = no,
+    DiffQuoteLevels = set.init,
+    Context0 = append_text_context(QuoteLevel, DiffLine, DiffQuoteLevels),
+    append_text(Max, String, Start, LastBreak, Cur, Context0, _Context,
+        cord.init, LinesCord),
+    Lines = cord.list(LinesCord).
+
+:- pred append_text(int::in, string::in, int::in, int::in, int::in,
+    append_text_context::in, append_text_context::out,
+    cord(pager_text)::in, cord(pager_text)::out) is det.
+
+append_text(Max, String, Start, LastBreak, Cur, !Context, !Lines) :-
+    ( string.unsafe_index_next(String, Cur, Next, Char) ->
+        (
+            Char = '\n'
+        ->
+            append_substring(String, Start, Cur, !Context, !Lines),
+            reset_context(!Context),
+            append_text(Max, String, Next, Next, Next, !Context, !Lines)
+        ;
+            char.is_whitespace(Char)
+        ->
+            append_text(Max, String, Start, Cur, Next, !Context, !Lines)
+        ;
+            % Wrap long lines.
+            % XXX this should actually count with wcwidth
+            Next - Start > Max
+        ->
+            maybe_append_substring(String, Start, LastBreak, !Context, !Lines),
+            skip_whitespace(String, LastBreak, NextStart),
+            append_text(Max, String, NextStart, NextStart, Next, !Context,
+                !Lines)
+        ;
+            append_text(Max, String, Start, LastBreak, Next, !Context, !Lines)
+        )
+    ;
+        % End of string.
+        maybe_append_substring(String, Start, Cur, !Context, !Lines)
+    ).
+
+:- pred reset_context(append_text_context::in, append_text_context::out)
+    is det.
+
+reset_context(!Context) :-
+    !Context ^ atc_cur_quote_level := no,
+    !Context ^ atc_cur_line_type := no.
+
+:- pred maybe_append_substring(string::in, int::in, int::in,
+    append_text_context::in, append_text_context::out,
+    cord(pager_text)::in, cord(pager_text)::out) is det.
+
+maybe_append_substring(String, Start, End, !Context, !Lines) :-
+    ( End > Start ->
+        append_substring(String, Start, End, !Context, !Lines)
+    ;
+        true
+    ).
+
+:- pred append_substring(string::in, int::in, int::in,
+    append_text_context::in, append_text_context::out,
+    cord(pager_text)::in, cord(pager_text)::out) is det.
+
+append_substring(String, Start, End, Context0, Context, !Lines) :-
+    Context0 = append_text_context(MaybeQuoteLevel0, MaybeLineType0,
+        DiffQuoteLevels0),
+    string.unsafe_between(String, Start, End, SubString),
+    (
+        MaybeQuoteLevel0 = yes(QuoteLevel),
+        QuoteMarkerEnd = 0
+    ;
+        MaybeQuoteLevel0 = no,
+        detect_quote_level(SubString, 0, QuoteLevel, QuoteMarkerEnd)
+    ),
+    (
+        MaybeLineType0 = yes(diff(DiffLine))
+    ->
+        % Maintain diff line type across wrapped diff lines.
+        LineType = diff(DiffLine),
+        DiffQuoteLevels = DiffQuoteLevels0
+    ;
+        ( contains(DiffQuoteLevels0, QuoteLevel) ->
+            (
+                QuoteLevel = 0,
+                MaybeLineType0 = no,
+                detect_diff_end(String, Start)
+            ->
+                PrevDiff = no,
+                delete(QuoteLevel, DiffQuoteLevels0, DiffQuoteLevels1)
+            ;
+                PrevDiff = yes,
+                DiffQuoteLevels1 = DiffQuoteLevels0
+            )
+        ;
+            PrevDiff = no,
+            DiffQuoteLevels1 = DiffQuoteLevels0
+        ),
+        (
+            % Only detect diffs at start of line.
+            MaybeLineType0 = no,
+            detect_diff(String, Start + QuoteMarkerEnd, End, PrevDiff,
+                QuoteLevel, DiffLine)
+        ->
+            LineType = diff(DiffLine),
+            insert(QuoteLevel, DiffQuoteLevels1, DiffQuoteLevels)
+        ;
+            PrevDiff = yes
+        ->
+            LineType = diff(diff_common),
+            DiffQuoteLevels = DiffQuoteLevels0
+        ;
+            detect_url(SubString, UrlStart, UrlEnd)
+        ->
+            LineType = url(UrlStart, UrlEnd),
+            DiffQuoteLevels = DiffQuoteLevels0
+        ;
+            LineType = plain,
+            DiffQuoteLevels = DiffQuoteLevels0
+        )
+    ),
+    Text = pager_text(QuoteLevel, SubString, QuoteMarkerEnd, LineType),
+    % We don't really need cords; a reverse list would do.
+    snoc(!.Lines, Text) = !:Lines,
+    Context = append_text_context(yes(QuoteLevel), yes(LineType),
+        DiffQuoteLevels).
+
+%-----------------------------------------------------------------------------%
+
+:- pred detect_quote_level(string::in, int::in, int::out, int::out) is det.
+
+detect_quote_level(String, Pos0, QuoteLevel, QuoteMarkerEnd) :-
+    ( quote_marker(String, Pos0, Pos1) ->
+        detect_quote_level(String, Pos1, QuoteLevel0, QuoteMarkerEnd),
+        QuoteLevel = 1 + QuoteLevel0
+    ;
+        QuoteLevel = 0,
+        QuoteMarkerEnd = Pos0
+    ).
+
+:- pred quote_marker(string::in, int::in, int::out) is semidet.
+
+quote_marker(String, !Pos) :-
+    string.unsafe_index_next(String, !Pos, Char),
+    (
+        Char = ('>')
+    ;
+        % Accept a single optional space before the quote character.
+        Char = (' '),
+        string.unsafe_index_next(String, !Pos, '>')
+    ).
+
+:- pred detect_diff(string::in, int::in, int::in, bool::in, quote_level::in,
+    diff_line::out) is semidet.
+
+detect_diff(String, I, CurLineEnd, PrevDiff, QuoteLevel, Diff) :-
+    string.unsafe_index_next(String, I, J, Char),
+    (
+        Char = ('+'),
+        (
+            PrevDiff = yes
+        ;
+            QuoteLevel > 0,
+            skip_nls(String, CurLineEnd, NextLineOffset),
+            lookahead_likely_diff(String, NextLineOffset, QuoteLevel)
+        ),
+        Diff = diff_add
+    ;
+        Char = ('-'),
+        ( J = CurLineEnd ->
+            true
+        ;
+            not dashes_or_sig_separator(String, J, CurLineEnd)
+        ),
+        (
+            PrevDiff = yes
+        ;
+            QuoteLevel > 0,
+            skip_nls(String, CurLineEnd, NextLineOffset),
+            lookahead_likely_diff(String, NextLineOffset, QuoteLevel)
+        ),
+        Diff = diff_rem
+    ;
+        Char = ('@'),
+        unsafe_substring_prefix(String, I, "@@ "),
+        Diff = diff_hunk
+    ;
+        Char = 'd',
+        unsafe_substring_prefix(String, I, "diff -"),
+        Diff = diff_index
+    ;
+        Char = 'I',
+        unsafe_substring_prefix(String, I, "Index: "),
+        Diff = diff_index
+    ).
+
+    % Quoted diffs are often missing the standard diff headers.  To help
+    % detect diff lines without diff headers, we look ahead one line (only).
+    %
+:- pred lookahead_likely_diff(string::in, int::in, quote_level::in) is semidet.
+
+lookahead_likely_diff(String, Start, ExpectedQuoteLevel) :-
+    detect_quote_level(String, Start, ExpectedQuoteLevel, QuoteMarkerEnd),
+    I = QuoteMarkerEnd,
+    string.unsafe_index_next(String, I, _, Char),
+    (
+        Char = ('+')
+    ;
+        Char = ('-')
+    ;
+        Char = ('@'),
+        unsafe_substring_prefix(String, I, "@@ ")
+    ;
+        Char = 'd',
+        unsafe_substring_prefix(String, I, "diff -")
+    ;
+        Char = 'I',
+        unsafe_substring_prefix(String, I, "Index: ")
+    ).
+
+:- pred dashes_or_sig_separator(string::in, int::in, int::in) is semidet.
+
+dashes_or_sig_separator(String, I0, End) :-
+    ( I0 < End ->
+        string.unsafe_index_next(String, I0, I1, Char),
+        (
+            Char = ('-'),
+            dashes_or_sig_separator(String, I1, End)
+        ;
+            Char = (' '),
+            I1 = End
+        )
+    ;
+        true
+    ).
+
+:- pred skip_nls(string::in, int::in, int::out) is det.
+
+skip_nls(String, I0, I) :-
+    ( string.unsafe_index_next(String, I0, I1, Char) ->
+        ( Char = ('\n') ->
+            skip_nls(String, I1, I)
+        ;
+            I = I0
+        )
+    ;
+        I = I0
+    ).
+
+:- pred detect_diff_end(string::in, int::in) is semidet.
+
+detect_diff_end(String, Start) :-
+    % At quote level zero diffs are likely posted in full and have a limited
+    % set of initial characters.
+    string.unsafe_index_next(String, Start, _, Char),
+    Char \= ('+'),
+    Char \= ('-'),
+    Char \= (' '),
+    Char \= ('@'),
+    Char \= ('d'),
+    Char \= ('I'),
+    Char \= ('i').
+
+%-----------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sts=4 sw=4 et
