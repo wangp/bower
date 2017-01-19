f8e52e7dea39122205d5948d2d0d34888383acb8
Stian Soiland-Reyes <stain@apache.org>
2017-01-19 13:55:36 +0000

    unarchive in thread view
    ..but for some reason the tags don't update on screen
    after A without pressing = -- unlike a. Why?

--- src/thread_pager.m
+++ src/thread_pager.m
@@ -917,6 +917,12 @@ thread_pager_input(Key, Action, MessageUpdate, !Info) :-
         next_message(MessageUpdate, !Info),
         Action = continue
     ;
+        Key = char('A')
+    ->
+        unarchive(!Info),
+        next_message(MessageUpdate, !Info),
+        Action = continue
+    ;
         Key = char('d')
     ->
         toggle_deleted(deleted, !Info),
@@ -1323,6 +1329,20 @@ archive(!Info) :-
         Line1 = Line0 ^ tp_curr_tags := TagSet,
         Line = Line1 ^ tp_std_tags ^ unread := read,
         set_cursor_line(Line, Scrollable0, Scrollable),
+        !Info ^ tp_scrollable := Scrollable
+    ;
+        true
+    ).
+
+:- pred unarchive(thread_pager_info::in, thread_pager_info::out) is det.
+
+unarchive(!Info) :-
+    Scrollable0 = !.Info ^ tp_scrollable,
+    ( get_cursor_line(Scrollable0, _Cursor, Line0) ->
+        TagSet0 = Line0 ^ tp_curr_tags,
+        set.insert(tag("inbox"), TagSet0, TagSet),
+        Line1 = Line0 ^ tp_curr_tags := TagSet,
+        set_cursor_line(Line1, Scrollable0, Scrollable),
         !Info ^ tp_scrollable := Scrollable
     ;
         true
