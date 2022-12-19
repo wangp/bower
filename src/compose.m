wangp/bower

commit c4521186e9be40e7982019cdacc207671760241d

Allow manual overrides of the date header field
Previously, the date field was always updated when sending the
message. While this behaviour is quite reasonable in most cases,
there might also be moments when specifying a date different from
the system's current local time might come in handy. This commit
introduces the ability to override the date field by simply including
it in the temporary message. If a non-empty date header is included
in the temporary message, bower will use the user-supplied value
when sending the email. If no date header is included (the default)
or if the date header is empty (i. e. consists only of zero or more
spaces) bower will continue to use the system's current local time
to automatically provide a value for the date field.

This change follows the unix philosophy of always giving users
enough rope to hang themselves; and then to add a couple of feet
more just in case. I. e. a user-supplied date value is used as-is,
without any kinds of integrity checks. This gives users quite some
freedom when using manual overrides, but it also presumes that they
know what they are doing. The date override is also not documented
(yet), but I believe that the behaviour is quite intuitive for the
kinds of users who would want to use it.

Note that bower also automatically sets a date when postponing
messages. This date will currently also appear in the header when
the postponed message is resumed. In order to only include this
date if it was explicitly specified by the user, we would need to
introduce some means of preserving bower-specific metadata across
the postpone-resume cycle. This change will be left for a later
commit.

src/compose.m
@@ -141,7 +141,8 @@
                 ph_to           :: address_list,
                 ph_cc           :: address_list,
                 ph_bcc          :: address_list,
-                ph_replyto      :: address_list
+                ph_replyto      :: address_list,
+                ph_date         :: maybe(string)
             ).
 
 :- type attach_info == scrollable(attachment).
@@ -619,10 +620,20 @@ create_edit_stage_2(Config, Screen, Headers0, Text0, UseAltHtmlFilter,
         Transition = screen_transition(not_sent, set_warning(Error))
     ).
 
+:- pred parse_date_header(header_value::in, maybe(string)::out) is det.
+
+parse_date_header(Date, ParsedDate) :-
+    ParsedDate0 = header_value_string(Date),
+    ( string.all_match(char.is_whitespace, ParsedDate0) ->
+        ParsedDate = no
+    ;
+        ParsedDate = yes(ParsedDate0)
+    ).
+
 :- pred make_parsed_headers(headers::in, parsed_headers::out) is det.
 
 make_parsed_headers(Headers, Parsed) :-
-    Headers = headers(_Date, From, To, Cc, Bcc, _Subject, ReplyTo,
+    Headers = headers(Date, From, To, Cc, Bcc, _Subject, ReplyTo,
         _References, _InReplyTo, _Rest),
 
     % [RFC 6854] allows group syntax in From - saves us work.
@@ -632,9 +643,10 @@ make_parsed_headers(Headers, Parsed) :-
     parse_address_list(Opt, header_value_string(Cc), ParsedCc),
     parse_address_list(Opt, header_value_string(Bcc), ParsedBcc),
     parse_address_list(Opt, header_value_string(ReplyTo), ParsedReplyTo),
+    parse_date_header(Date, ParsedDate),
 
     Parsed = parsed_headers(ParsedFrom, ParsedTo, ParsedCc, ParsedBcc,
-        ParsedReplyTo).
+        ParsedReplyTo, ParsedDate).
 
 :- pred call_editor(prog_config::in, string::in, call_res::out, io::di, io::uo)
     is det.
@@ -815,11 +827,12 @@ parse_and_expand_headers(Config, Headers0, Headers, Parsed, !IO) :-
     Expand(Cc0, Cc, ParsedCc, !IO),
     Expand(Bcc0, Bcc, ParsedBcc, !IO),
     Expand(ReplyTo0, ReplyTo, ParsedReplyTo, !IO),
+    parse_date_header(Date, ParsedDate),
 
     Headers = headers(Date, From, To, Cc, Bcc, Subject, ReplyTo,
         References, InReplyTo, Rest),
     Parsed = parsed_headers(ParsedFrom, ParsedTo, ParsedCc, ParsedBcc,
-        ParsedReplyTo).
+        ParsedReplyTo, ParsedDate).
 
 :- pred parse_and_expand_addresses(prog_config::in, quote_opt::in,
     header_value::in, header_value::out, address_list::out, io::di, io::uo)
@@ -2595,17 +2608,24 @@ get_addr_spec_in_mailbox(Mailbox, AddrSpec) :-
 make_headers(Prepare, Headers, ParsedHeaders, Date, MessageId, WriteHeaders) :-
     Headers = headers(_Date, _From, _To, _Cc, _Bcc, Subject, _ReplyTo,
         InReplyTo, References, RestHeaders),
-    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, ReplyTo),
+    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, ReplyTo, ExplicitDate),
     some [!Acc] (
         !:Acc = [],
         (
-            ( Prepare = prepare_send
-            ; Prepare = prepare_postpone
-            ),
-            cons(header(field_name("Date"), unstructured(Date, no_encoding)),
-                !Acc)
+            ExplicitDate = yes(DateValue0),
+            DateValue = unstructured(header_value(DateValue0), no_encoding),
+            cons(header(field_name("Date"), DateValue), !Acc)
         ;
-            Prepare = prepare_edit(_)
+            ExplicitDate = no,
+            (
+                ( Prepare = prepare_send
+                ; Prepare = prepare_postpone
+                ),
+                cons(header(field_name("Date"),
+                            unstructured(Date, no_encoding)), !Acc)
+            ;
+                Prepare = prepare_edit(_)
+            )
         ),
         (
             Prepare = prepare_send,

src/message_file.m
@@ -186,8 +186,7 @@ add_standard_header(Name, Value, !Headers) :-
     ; strcase_equal(Name, "References") ->
         !Headers ^ h_references := Value
     ; strcase_equal(Name, "Date") ->
-        % Ignore it.
-        true
+        !Headers ^ h_date := Value
     ; strcase_equal(Name, "Message-ID") ->
         % Ignore it.
         true

src/compose.crypto.m
@@ -97,7 +97,7 @@ maintain_encrypt_keys(ParsedHeaders, !CryptoInfo, !IO) :-
     io::di, io::uo) is det.
 
 maintain_encrypt_keys_map(Crypto, ParsedHeaders, !EncryptKeys, !IO) :-
-    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, _ReplyTo),
+    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, _ReplyTo, _Date),
     Addresses = From ++ To ++ Cc ++ Bcc,
     solutions(addr_specs(Addresses), AddrSpecs),
     list.foldl2(maintain_encrypt_key(Crypto), AddrSpecs, !EncryptKeys, !IO).
@@ -140,7 +140,7 @@ maintain_sign_keys(ParsedHeaders, !CryptoInfo, !IO) :-
     io::di, io::uo) is det.
 
 maintain_sign_keys_map(Crypto, ParsedHeaders, !SignKeys, !IO) :-
-    ParsedHeaders = parsed_headers(From, _To, _Cc, _Bcc, _ReplyTo),
+    ParsedHeaders = parsed_headers(From, _To, _Cc, _Bcc, _ReplyTo, _Date),
     solutions(addr_specs(From), AddrSpecs),
     list.foldl2(maintain_sign_key(Crypto), AddrSpecs, !SignKeys, !IO).
 
@@ -249,7 +249,7 @@ is_valid_userid_matching_email(Email, IgnoreCase, UserId) :-
 get_encrypt_keys(CryptoInfo, ParsedHeaders, EncryptForWhom, SelectedKeys,
         Missing, LeakedBccs) :-
     EncryptKeys = CryptoInfo ^ ci_encrypt_keys,
-    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, _ReplyTo),
+    ParsedHeaders = parsed_headers(From, To, Cc, Bcc, _ReplyTo, _Date),
     (
         EncryptForWhom = from_only,
         Addresses = From
@@ -275,7 +275,7 @@ get_encrypt_keys(CryptoInfo, ParsedHeaders, EncryptForWhom, SelectedKeys,
 
 get_sign_keys(CryptoInfo, ParsedHeaders, SelectedKeys) :-
     SignKeys = CryptoInfo ^ ci_sign_keys,
-    ParsedHeaders = parsed_headers(From, _To, _Cc, _Bcc, _ReplyTo),
+    ParsedHeaders = parsed_headers(From, _To, _Cc, _Bcc, _ReplyTo, _Date),
     solutions(addr_specs(From), AddrSpecs),
     list.foldl2(get_key(SignKeys), AddrSpecs,
         [], RevSelectedKeys, [], _RevMissing),

commit 0cd32bb2e6776fbbdc05c4670b3bf74ae58e44d2

Retain date override state across postpone-resume
This introduces a custom header field called X-Bower-Metadata.
X-Bower-Metadata will be written into the header of postponed
messages if needed and it will be read back and removed from the
header when parsing postponed messages. The custom header field is
set up in a way that it could potentially store any number of boolean
options and maybe even arbitrary key-value-pairs. Its structure is a
list of strings separated by semicolons. Currently, all values except
for 'retain_date' are ignored. The value 'retain_date' will be stored
in X-Bower-Metadata if the date header was manually set by the user.

If X-Bower-Metadata does not specify 'retain_date', the date header
will be cleared upon opening a postponed message. This commit thus
restores the previous behaviour for resuming postponed messages when
the manual date override is not used, resolving the issue introduced
in commit c4521186.

src/compose.m
@@ -529,12 +529,19 @@ continue_from_message(Config, Crypto, Screen, ContinueBase, Message,
         CallRes, !IO),
     (
         CallRes = ok(String),
-        parse_message(String, HeadersB, _Body),
+        parse_message(String, HeadersB, _Body, PostponedMetadata),
         some [!Headers] (
             !:Headers = Headers0,
             !Headers ^ h_replyto := HeadersB ^ h_replyto,
             !Headers ^ h_inreplyto := HeadersB ^ h_inreplyto,
             !Headers ^ h_references := HeadersB ^ h_references,
+            ( ContinueBase = postponed_message ->
+                ( PostponedMetadata ^ retain_date = clear_date,
+                  !Headers ^ h_date := header_value("")
+                ;
+                  PostponedMetadata ^ retain_date = retain_date
+                )
+            ; true ),
             Headers = !.Headers
         ),
         (
@@ -600,7 +607,8 @@ create_edit_stage_2(Config, Screen, Headers0, Text0, UseAltHtmlFilter,
             ResEdit = ok,
             parse_message_file(Filename, ResParse, !IO),
             (
-                ResParse = ok(Headers1 - Text),
+                ResParse = ok(parsed_message(Headers1, Text,
+                                             _PostponedMetadata)),
                 io.remove_file(Filename, _, !IO),
                 update_references(Headers0, Headers1, Headers2),
                 reenter_staging_screen(Config, Screen, Headers2, Text,
@@ -2638,9 +2646,24 @@ make_headers(Prepare, Headers, ParsedHeaders, Date, MessageId, WriteHeaders) :-
                 unstructured(wrap_angle_brackets(MessageId), no_encoding)),
                 !Acc)
         ;
-            ( Prepare = prepare_edit(_)
-            ; Prepare = prepare_postpone
-            )
+            Prepare = prepare_postpone,
+            ( ParsedHeaders ^ ph_date = yes(_) ->
+              RetainDate = retain_date
+            ; RetainDate = clear_date ),
+            (
+                % This is a simple and straightforward solution for
+                % setting a single metadata value if needed. Should any
+                % other metadata values be added later, this code would
+                % need to be adjusted accordingly.
+                RetainDate = retain_date
+            ->
+                Metadata = "retain_date",
+                cons(header(field_name("X-Bower-Metadata"),
+                            unstructured(header_value(Metadata),
+                            no_encoding)), !Acc)
+            ; true )
+        ;
+            Prepare = prepare_edit(_)
         ),
         (
             ( Prepare = prepare_send

src/data.m
@@ -70,6 +70,13 @@
 :- type message_part_id
     --->    message_part_id(message_id, part_id).
 
+:- type parsed_message
+    --->    parsed_message(
+                headers         :: headers,
+                body            :: string,
+                metadata        :: postponed_metadata
+            ).
+
 :- type headers
     --->    headers(
                 % Technically, header fields.
@@ -93,6 +100,15 @@
     ;       decoded_unstructured(string).
             % An unstructured field that may contain RFC 2047 encoded-words,
             % which we keep in decoded form.
+
+:- type postponed_metadata
+    --->    postponed_metadata(
+                retain_date     :: pm_retain_date
+            ).
+
+:- type pm_retain_date
+    --->    retain_date;
+            clear_date.
 
 :- type tag
     --->    tag(string).

src/message_file.m
@@ -5,16 +5,16 @@
 :- interface.
 
 :- import_module io.
-:- import_module pair.
 
 :- import_module data.
 
 %-----------------------------------------------------------------------------%
 
-:- pred parse_message_file(string::in, io.res(pair(headers, string))::out,
+:- pred parse_message_file(string::in, io.res(parsed_message)::out,
     io::di, io::uo) is det.
 
-:- pred parse_message(string::in, headers::out, string::out) is det.
+:- pred parse_message(string::in, headers::out, string::out,
+    postponed_metadata::out) is det.
 
 %-----------------------------------------------------------------------------%
 %-----------------------------------------------------------------------------%
@@ -41,8 +41,8 @@ parse_message_file(Filename, Res, !IO) :-
         read_file_as_string(Stream, ResRead, !IO),
         (
             ResRead = ok(String),
-            parse_message(String, Headers, Body),
-            Res = ok(Headers - Body)
+            parse_message(String, Headers, Body, PostponedMetadata),
+            Res = ok(parsed_message(Headers, Body, PostponedMetadata))
         ;
             ResRead = error(_, Error),
             Res = error(Error)
@@ -53,15 +53,17 @@ parse_message_file(Filename, Res, !IO) :-
         Res = error(Error)
     ).
 
-parse_message(String, Headers, Body) :-
-    read_headers(String, 0, Pos0, init_headers, Headers),
+parse_message(String, Headers, Body, PostponedMetadata) :-
+    DefaultMetadata = postponed_metadata(clear_date),
+    read_headers(String, 0, Pos0, init_headers, Headers, DefaultMetadata,
+                 PostponedMetadata),
     skip_blank_line(String, Pos0, Pos1),
     string.unsafe_between(String, Pos1, count_code_units(String), Body).
 
-:- pred read_headers(string::in, int::in, int::out, headers::in, headers::out)
-    is det.
+:- pred read_headers(string::in, int::in, int::out, headers::in, headers::out,
+    postponed_metadata::in, postponed_metadata::out) is det.
 
-read_headers(String, !Pos, !Headers) :-
+read_headers(String, !Pos, !Headers, !PostponedMetadata) :-
     ( read_header_field(String, !Pos, Name, RawValue) ->
         % Other headers should be decoded as well.
         ( strcase_equal(Name, "Subject") ->
@@ -70,10 +72,32 @@ read_headers(String, !Pos, !Headers) :-
         ;
             Value = header_value(RawValue)
         ),
-        add_header(Name, Value, !Headers),
-        read_headers(String, !Pos, !Headers)
+        ( strcase_equal(Name, "X-Bower-Metadata") ->
+            read_postponed_metadata(Value, !PostponedMetadata)
+        ;
+            add_header(Name, Value, !Headers)
+        ),
+        read_headers(String, !Pos, !Headers, !PostponedMetadata)
     ;
         true
+    ).
+
+:- pred read_postponed_metadata(header_value::in, postponed_metadata::in,
+    postponed_metadata::out) is det.
+
+read_postponed_metadata(HeaderValue, Metadata0, Metadata) :-
+    MetadataString = header_value_string(HeaderValue),
+    MetadataList = list.map(lstrip, list.map(rstrip,
+                            split_at_char(';', MetadataString))),
+    list.foldl(read_metadata_field, MetadataList, Metadata0, Metadata).
+
+:- pred read_metadata_field(string::in, postponed_metadata::in,
+    postponed_metadata::out) is det.
+
+read_metadata_field(Field, !Metadata) :-
+    ( strcase_equal(Field, "retain_date") ->
+        !Metadata ^ retain_date := retain_date
+    ; true  % Ignore any unknown values
     ).
 
 :- pred read_header_field(string::in, int::in, int::out,